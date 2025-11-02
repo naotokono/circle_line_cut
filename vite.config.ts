23f55d16b2442238423c60a5dd9b0427f672797e
kono <kono@kono.local>
2025-11-02 17:02:16 +0900

    エディタ設定やビルド設定の微調整（ vite に寄せた）

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from "vite";
 export default defineConfig({
   server: {
     port: 5173,
+    hmr: false,
   },
   build: {
     target: "es2023",
-    sourcemap: false,           // 本番はソースマップなし
-    minify: "esbuild",          // ここはVite内部で使うだけ。別インストール不要
+    sourcemap: false,
+    minify: "esbuild",
   }
 });
